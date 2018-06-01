c3d35093b310844a05e29b1ea66582beb5f3ca02
Ornery <ornerygames@gmail.com>
2018-06-01 18:32:54 -0300

    ATT
    ATT

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
 exports.run = async(client, message, args) => {
         client.user.setPresence({ game: { name: "Anything", type:  1} });
+        client.user.setPresence({ game: { name: "Sendo programado pelo ornery!", type:  1} });
 }
