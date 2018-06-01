40414d85b13f271e7419e6475f8b40b75d1533f8
Ornery <ornerygames@gmail.com>
2018-06-01 20:07:37 -0300

    att
    att

--- commands/criador.js
+++ commands/criador.js
@@ -3,7 +3,7 @@ exports.run = (client , message, args) =>{
     var CriadorEmbed = new Discord.RichEmbed()
             .setColor('#4b42f4')
             .setDescription('Criador ๖̶̶̶ۣۣۜۜ͜ζ͜͡オリリくん (Ornery)#6770')
-            .setDescription('Colaboração de Vinni#3130')
+            
 
 
 message.channel.sendMessage(CriadorEmbed);
